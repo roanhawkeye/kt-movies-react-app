a3b1c5b6a8302be5b9939b7bd226f3acb7577931
Daniel Martinez <daniel.martinez@mckesson.com>
2022-04-07 13:54:15 -0500

    Add webpack build prod script

---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const envAlias = process.env.ENV || "PROD";
+
+const { parsed: env } = require("dotenv").config({ path: `.env.${envAlias}` });
+
+module.exports = {
+  mode: "production",
+
+  entry: "./src/index.ts",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, `builds/${envAlias}`),
+    publicPath: env.BASE_URL_NAME,
+  },
+
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlPlugin({
+      template: path.resolve(__dirname, "index.template.html"),
+    }),
+  ],
+};
