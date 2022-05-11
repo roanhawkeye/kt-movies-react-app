7f24f14a694af7324e869260a8bb2c4d117a07c2
Martinez, Daniel <roanhawkeye@gmail.com>
2022-05-11 16:09:09 -0500

    Refactor router to root and search page as private

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,11 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import {
-  Switch,
-  Redirect,
-  Route,
-  Router,
-} from 'react-router-dom';
+import { Switch, Redirect, Router } from 'react-router-dom';
 
 
 import { history } from './history';
 import { Home } from './screens/home';
-import { LogInRoute } from './Routes';
+import { LogInRoute, PrivateRoute } from './Routes';
 import { useSelector } from 'react-redux';
 import { selectAuthStatus } from './store/auth';
 import { appRoutes } from './appRoutes';
@@ -29,10 +24,10 @@ export const AppRouter: FC = () => {
       <AppContainer>
         <Switch>
           <LogInRoute status={status} path={appRoutes.login} component={Auth} />
-          <Route path={appRoutes.search} exact component={Home} />
-          <Route path={appRoutes.root}>
+          <PrivateRoute status={status} path={appRoutes.search} exact component={Home} />
+          <PrivateRoute status={status} path={appRoutes.root}>
             <Redirect to={appRoutes.search} />
-          </Route>
+          </PrivateRoute>
         </Switch>
       </AppContainer>
     </Router>
